91a05576863945679c3e87f0a4160bcd704fbf75
ReaL4Lex <rasaq.ca@gmail.com>
2025-11-30 12:29:22 -0500

    Use Heroku env vars for MongoDB and session

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Visit: http://localhost:${PORT}`);
 });
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.MONGODB_URI,   // 👈 IMPORTANT
+  collectionName: 'sessions',          // optional
+});
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,  // 👈 IMPORTANT
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,      // 1 day
+  },
+}));
